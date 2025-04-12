166a61674a82563bb074beb3fe9cd77a670367ae
Simon Courtois <scourtois_github@cubyx.fr>
2025-04-12 22:42:58 +0200

    Removing not working custom API call

--- src/lib/actions/index.ts
+++ src/lib/actions/index.ts
@@ -1,5 +1,4 @@
 export { copyMail } from './copy-mail';
-export { customApiCall } from './custom-api-call';
 export { deleteMail } from './delete-mail';
 export { moveMail } from './move-mail';
 export { updateMail } from './update-mail';
