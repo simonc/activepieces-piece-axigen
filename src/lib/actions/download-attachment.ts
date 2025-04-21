ee1281af459679728eb90e8f0d185f9f3e5e8b78
Simon Courtois <scourtois_github@cubyx.fr>
2025-04-21 10:52:32 +0200

    Allowing attachment download to return base64 content

--- src/lib/actions/download-attachment.ts
+++ src/lib/actions/download-attachment.ts
@@ -18,10 +18,30 @@ export const downloadAttachment = createAction({
       description: 'The ID of the attachment to download',
       required: true,
     }),
+    outputFormat: Property.StaticDropdown({
+      displayName: 'Output Format',
+      description: 'The format of the output',
+      required: true,
+      options: {
+        options: [
+          { label: 'Original', value: 'original' },
+          { label: 'Base64', value: 'base64' },
+        ],
+      },
+      defaultValue: 'original',
+    }),
   },
   async run({ auth, propsValue }) {
     const client = makeClient(auth);
-    const { mailId, attachmentId } = propsValue;
-    return await client.downloadAttachment(mailId, attachmentId);
+    const { mailId, attachmentId, outputFormat } = propsValue;
+    const content = await client.downloadAttachment(mailId, attachmentId);
+
+    if (outputFormat === 'original') {
+      return content;
+    }
+
+    const base64content = Buffer.from(await content.arrayBuffer()).toString('base64');
+
+    return base64content;
   },
 });
